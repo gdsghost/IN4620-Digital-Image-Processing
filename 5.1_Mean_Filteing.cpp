6704d881d788c3e0350f0f50725cb3fcac8aa537
Sudam Yasodya <sudam19961994@gmail.com>
2022-11-10 20:10:30 +0530

    kernal value from comman line

--- 5.1_Mean_Filteing.cpp
+++ 5.1_Mean_Filteing.cpp
@@ -6,6 +6,10 @@ using namespace cv;
 using namespace std;
 
 int main(int argc, char* argv[]) {
+	if (argc != 3) {
+		printf("Enter exeFileName ImageName kernamSize");
+		return -1;
+	}
 
 	Mat image = imread(argv[1]);
 
@@ -20,18 +24,23 @@ int main(int argc, char* argv[]) {
 
 	Mat output = gray.clone();
 
+	//get the kernal size using command line and adjesment value
+	int k = atoi(argv[2]);
+	int adj = k / 2;
+
+	int sum;
+	int avg = 0;
 	// we should write 2 for loops to reach each and every pixel of the image
-	for (int i = 1; i < image.rows - 1; i++) {
-		for (int j = 1; j < image.cols - 1; j++) {
+	for (int i = adj; i < image.rows - adj; i++) {
+		for (int j = adj; j < image.cols - adj; j++) {
 			// then we have to loop the kernal to get the total of neighbouring pixels to calculate the average and place to this selected pixel
-			int sum = 0;
-			int avg = 0;
-			for (int g = i - 1; g <= i + 1; g++) {
-				for (int h = j - 1; h <= j + 1; h++) {
+			sum = 0;
+			for (int g = i - adj; g <= i + adj; g++) {
+				for (int h = j - adj; h <= j + adj; h++) {
 					sum = sum + (int)gray.at<uchar>(g, h);
 				}
 			}
-			avg = sum / 9;
+			avg = sum / (k * k);
 			output.at<uchar>(i, j) = (uchar)avg;
 		}
 	}
