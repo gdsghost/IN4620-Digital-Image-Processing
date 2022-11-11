42ce9d90c060c8dfaed4db99890ebced86aed6a7
Tharaka9619 <57471664+Ttecs@users.noreply.github.com>
2022-11-11 08:03:37 +0530

    created gausian with sobel

---
+++ Gausian_with_Sobel_operator.cpp
@@ -0,0 +1,177 @@
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core.hpp"
+
+using namespace cv;
+using namespace std;
+
+
+
+int main(int argc, char* argv[]) {
+	Mat img = imread(argv[1]);
+	if (!img.data) {
+		cout << "Could not load image" << endl;
+		waitKey(0);
+		return -1;
+	}
+	//Gray image and initialization
+	Mat gr;
+	int his[256];
+	int his2[256];
+
+	for (int i = 0; i < 256; i++) {
+		his[i] = 0;
+		his2[i] = 0;
+	}
+	//Gray conversion
+	cvtColor(img, gr, CV_BGR2GRAY);
+
+	int h = gr.rows;
+	int w = gr.cols;
+	int val = 0;
+	int max = 0;
+
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			val = gr.at<uchar>(i, j);
+			his[val]++;
+		}
+	}
+	for (int i = 0; i < 256; i++) {
+		if (max < his[i]) {
+			max = his[i];
+		}
+	}
+
+	//Histogram for Original Image
+	Mat him(301, 260, CV_8UC1, Scalar(255));
+	int hist[256];
+	double maxd = max;
+	for (int i = 0; i < 256; i++) {
+		hist[i] = cvRound(double(his[i] / maxd) * 300);
+		Point pt1 = Point(i, 300 - hist[i]);
+		Point pt2 = Point(i, 300);
+		line(him, pt1, pt2, Scalar(0), 1, 8, 0);
+	}
+
+	//Gaussian Filter
+	Mat Gaus = gr.clone();
+	int kernel = 5;
+	// intialising standard deviation to 1.0 
+	double sigma = 1.0;
+	double r = 2.0 * sigma * sigma;
+	double s = 2.0 * sigma * sigma;
+
+	// sum is for normalization 
+	double sum = 0.0;
+	double GKernel[5][5];
+
+	// generating 5x5 kernel 
+	for (int x = -2; x <= 2; x++) {
+		for (int y = -2; y <= 2; y++) {
+			r = sqrt(x * x + y * y);
+			GKernel[x + 2][y + 2] = (exp(-(r * r) / s)) / (3.14 * s);
+			sum += GKernel[x + 2][y + 2];
+		}
+	}
+
+	// normalising the Kernel 
+	for (int i = 0; i < 5; ++i)
+		for (int j = 0; j < 5; ++j)
+			GKernel[i][j] /= sum;
+
+
+	for (int i = 0; i < 5; ++i) {
+		for (int j = 0; j < 5; ++j)
+			cout << GKernel[i][j] << "\t";
+		cout << endl;
+	}
+
+
+	for (int i = kernel / 2; i < h - (kernel / 2); i++) {
+		for (int j = kernel / 2; j < w - (kernel / 2); j++) {
+			int sum = 0;
+			for (int m = -kernel / 2; m <= kernel / 2; m++) {
+				for (int n = -kernel / 2; n <= kernel / 2; n++) {
+					int a = gr.at<uchar>(i + m, j + n);
+					double p = GKernel[2 + m][2 + n];
+					sum = sum + (int)(p * a);
+				}
+			}
+
+			Gaus.at<uchar>(i, j) = (uchar)sum;
+		}
+	}
+
+	//Sobel operator
+	Mat sob = Gaus.clone();
+	int Skernel = 3;
+	int elements = Skernel * Skernel;
+	int Gx = 0;
+	int Gy = 0;
+	int G = 0;
+	int T = 35;
+
+	for (int i = Skernel / 2; i < h - (Skernel / 2); i++) {
+		for (int j = Skernel / 2; j < w - (Skernel / 2); j++) {
+			Gx = (Gaus.at<uchar>(i - 1, j - 1) +
+				2 * Gaus.at<uchar>(i, j - 1) +
+				Gaus.at<uchar>(i + 1, j - 1) -
+				Gaus.at<uchar>(i - 1, j + 1) -
+				2 * Gaus.at<uchar>(i, j + 1) -
+				Gaus.at<uchar>(i + 1, j + 1));
+
+			Gy = (Gaus.at<uchar>(i - 1, j - 1) +
+				2 * Gaus.at<uchar>(i - 1, j) +
+				Gaus.at<uchar>(i - 1, j + 1) -
+				Gaus.at<uchar>(i + 1, j - 1) -
+				2 * Gaus.at<uchar>(i + 1, j) -
+				Gaus.at<uchar>(i + 1, j + 1));
+
+			G = abs(Gx) + abs(Gy);
+			if (G >= T) {
+				sob.at<uchar>(i, j) = 0;
+			}
+			else {
+				sob.at<uchar>(i, j) = 255;
+			}
+		}
+	}
+
+	//Pixel Calculation for Smoothed image
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			val = Gaus.at<uchar>(i, j);
+			his2[val] = his2[val] + 1;
+		}
+	}
+	int max2 = 0;
+	for (int i = 0; i < 256; i++) {
+		//cout << "Gray Level  " << i << " : " << his2[i] << endl;
+		if (max2 < his2[i]) {
+			max2 = his2[i];
+		}
+	}
+	//Histogram of Smoothed Image
+	Mat him2(301, 260, CV_8UC1, Scalar(255));
+	int hist2[256];
+	double maxd2 = max2;
+	for (int i = 0; i <= 255; i++) {
+		hist2[i] = cvRound(double(his2[i] / maxd2) * 300);
+		Point pt1 = Point(i, 300 - hist2[i]);
+		Point pt2 = Point(i, 300);
+		line(him2, pt1, pt2, Scalar(0), 1, 8, 0);
+	}
+
+	imshow("Image:", gr);
+	imshow("Histograam:", him);
+	imshow("Gausssian Smoothed", Gaus);
+	imshow("Gausssian Smoothed and Sobel ", sob);
+	imshow("New Histogram:", him2);
+
+	cvWaitKey(0);
+
+
+	return 0;
+
+}
